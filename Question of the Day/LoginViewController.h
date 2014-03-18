0bedf12e9c0c61cb449d91da844b9b6df2d22350
Ogny <ogny@Ognys-Mac.local>
2014-03-17 23:12:45 -0700

    Added login

---
+++ Question of the Day/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  QotDViewController.h
+//  Question of the Day
+//
+//  Created by Ogny on 3/17/14.
+//  Copyright (c) 2014 dognean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
